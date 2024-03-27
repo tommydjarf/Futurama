46353ed55df753e2cca3f837bca3e23ba8e1c0d3
Martin Persson <143416137+Ivan-Georg@users.noreply.github.com>
2024-03-27 11:26:08 +0100

    Gjort character chards clickable, modal med bild och 5 sayings

--- js/app.js
+++ js/app.js
@@ -1,87 +1,91 @@
+//////////
 async function printCharacters() {
-	let characters = await performDBOperation("characters", "readonly", "getAll");
+    let characters = await performDBOperation("characters", "readonly", "getAll");
 
-	// Remap the characters
-	characters = characters.map(remapCharacters);
+    // Remap the characters
+    characters = characters.map(remapCharacters);
 
-	const container = document.getElementsByClassName("characters-container")[0];
+    const container = document.getElementsByClassName("characters-container")[0];
 
-	for (const character of characters) {
-		const characterElement = document.createElement("div");
-		characterElement.className = "character-card";
-		characterElement.innerHTML = `
+    for (const character of characters) {
+        const characterElement = document.createElement("div");
+        characterElement.className = "character-card";
+        characterElement.innerHTML = `
 		<h3>${character.fullName}</h3>
 		<p>Occupation: ${character.occupation}</p>
 		<p>Homeplanet: ${character.homePlanet}</p>
+		<p>ID: ${character.id}</p>
 		`;
-		container.appendChild(characterElement);
-	}
+        characterElement.addEventListener("click", () => printCharacter(character.id));
+        container.appendChild(characterElement);
+    }
 }
 
-async function printCharacter() {
-	let test = document.getElementById("testButton");
-	let value = Number(test.dataset.id);
+async function printCharacter(id) {
+    let character = await performDBOperation("characters", "readonly", "get", id);
 
-	let character = await performDBOperation("characters", "readonly", "get", value);
+    // Remap the character
+    character = remapCharacters(character);
 
-	// Remap the character
-	character = remapCharacters(character);
+    let characterCard = document.querySelector("#characterModal .modal-character-card");
+    let randomSayings = getRandomSayings(character.sayings);
+    characterCard.innerHTML = `
+    <h3>${character.fullName}</h3>
+    <p>Occupation: ${character.occupation}</p>
+    <p>Homeplanet: ${character.homePlanet}</p>
+    <img src="${character.images.main}" alt="${character.fullName}">
+    <ul>
+        ${randomSayings.map((saying) => `<li>${saying}</li>`).join("")}
+    </ul>
+    `;
 
-	console.log(character);
-
-	let characterCard = document.querySelector("#characterModal .modal-character-card");
-	let randomSayings = getRandomSayings(character.sayings);
-	characterCard.innerHTML = `
-	<h3>${character.fullName}</h3>
-	<p>Occupation: ${character.occupation}</p>
-	<p>Homeplanet: ${character.homePlanet}</p>
-	<img src="${character.images.main}" alt="${character.fullName}">
-	<ul>
-		${randomSayings.map((saying) => `<li>${saying}</li>`).join("")}
-	</ul>
-	`;
-
-	let characterModal = document.getElementById("characterModal");
-	characterModal.style.display = "block";
+    let characterModal = document.getElementById("characterModal");
+    characterModal.style.display = "block";
 }
+/////////
 
 let closeButton = document.querySelector("#characterModal .close");
 closeButton.addEventListener("click", function () {
-	let characterModal = document.getElementById("characterModal");
-	characterModal.style.display = "none";
+    let characterModal = document.getElementById("characterModal");
+    characterModal.style.display = "none";
 });
 
 // Remap for characters
 function remapCharacters(character) {
-	const fullName = `${character.name.first || " "} ${character.name.middle || " "} ${character.name.last || " "}`;
-	return {
-		...character,
-		name: {
-			first: character.name.first || " ",
-			middle: character.name.middle || " ",
-			last: character.name.last || " ",
-		},
-		fullName: fullName,
-		occupation: character.occupation || "Slacker",
-		homePlanet: character.homePlanet || "The universe!",
-	};
+    const fullName = `${character.name.first || " "} ${character.name.middle || " "} ${
+        character.name.last || " "
+    }`;
+    return {
+        ...character,
+        name: {
+            first: character.name.first || " ",
+            middle: character.name.middle || " ",
+            last: character.name.last || " ",
+        },
+        fullName: fullName,
+        occupation: character.occupation || "Slacker",
+        homePlanet: character.homePlanet || "The universe!",
+    };
 }
 
 function getRandomSayings(sayings) {
-	let randomSayings = [];
-	for (let i = 0; i < 5; i++) {
-		let randomIndex = Math.floor(Math.random() * sayings.length);
-		randomSayings.push(sayings[randomIndex]);
-		sayings.splice(randomIndex, 1);
-	}
-	return randomSayings;
+    let randomSayings = [];
+    for (let i = 0; i < 5; i++) {
+        let randomIndex = Math.floor(Math.random() * sayings.length);
+        randomSayings.push(sayings[randomIndex]);
+        sayings.splice(randomIndex, 1);
+    }
+    return randomSayings;
 }
 
 window.onload = function () {
-	printCharacters();
+    printCharacters();
 
-	let testButton = document.getElementById("testButton");
-	testButton.addEventListener("click", printCharacter);
+    let closeButton = document.querySelector("#characterModal .close");
+    closeButton.addEventListener("click", function () {
+        let characterModal = document.getElementById("characterModal");
+        characterModal.style.display = "none";
+    });
 };
 
 // TODO: Add print function for characters/ edit function
